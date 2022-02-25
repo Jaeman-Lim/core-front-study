4c5be2455de70bd00d27faa5c7274e63705db642
Jaeman Lim <jaeman87@gmail.com>
2022-02-26 02:54:17 +0900

    sort(); function

--- JavaScript/sort.js
+++ JavaScript/sort.js
@@ -1,24 +1,43 @@
 // 문자열 정렬
 let fruit = ['orange', 'apple', 'banana'];
 fruit.sort();
-// console.log(fruit);
+console.log(fruit);
 
 // 숫자 정렬
 let score = [4, 11, 2, 10, 3, 1];
 
+/**
+ * 오름차순 메커니즘
+ * 반환 값 < 0 a가 b보다 앞에 있어야 한다.
+ * 반환 값 = 0 a와 b의 순서를 바꾸지 않는다.
+ * 반환 값 > 0 b가 a보다 앞에 있어야 한다.
+ */
+
+// 오름차순
 score.sort(function(a, b) {
-    return a-b; //오름차순
-    // return b-a; //내림차순
+    return a-b;
 });
-// console.log(score);
+// 내림차순
+score.sort(function(a, b) {
+    return b-a;
+})
 
 //숫자 정렬(함수형)
+
+// 오름차순
 score.sort(function(a, b) {
     if (a > b) return 1;
     if (a == b) return 0;
     if (a < b) return -1;
 });
-// console.log(score);
+//내림차순
+score.sort(function(a, b) {
+    if (a < b) return 1;
+    if (a == b) return 0;
+    if (a > b) return -1;
+})
+
+console.log(score);
 
 // 오브젝트 정렬
 let student = [
@@ -35,6 +54,7 @@ student.sort(function(a, b) {
 student.sort(function(a, b) {
     return a.name > b.name ? -1 : a.name < b.name ? 1 : 0   //내림차순
 })
+console.log(student);
 
 //나이순으로 정렬
 student.sort(function(a, b) {
@@ -42,10 +62,8 @@ student.sort(function(a, b) {
     if (a.age == b.age) return 0
     if (a.age < b.age) return 1
 })
-//오름차순
-//내림차순
-console.log(student);
 
+console.log(student);
 
 const arr = ['banana', 'b', 'Boy'];
 
@@ -55,5 +73,7 @@ arr.sort(function(a, b) {
 
     if(upperCaseA > upperCaseB) return 1;
     if(upperCaseA < upperCaseB) return -1;
-    if(upperCaseA === upperCaseB) return 0;
+    if(upperCaseA == upperCaseB) return 0;
 });
+
+console.log(arr);
